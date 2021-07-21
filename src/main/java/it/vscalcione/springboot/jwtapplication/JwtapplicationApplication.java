034c505a83336c9d5a1f5fe885612ddc0d004109
vscalcione <vincenzo99.scalcione@gmail.com>
2021-07-21 13:03:30 +0200

    Initial commit

---
+++ src/main/java/it/vscalcione/springboot/jwtapplication/JwtapplicationApplication.java
@@ -0,0 +1,13 @@
+package it.vscalcione.springboot.jwtapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/vscalcione/springboot/jwtapplication/JwtapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vscalcione.springboot.jwtapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
